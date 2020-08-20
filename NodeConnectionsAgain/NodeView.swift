5fa7ad6face9e141e7920649987d393e28d99f26
Johann Garces <hello@johannmg.com>
2020-08-19 19:39:58 -0700

    boxes drag

--- NodeConnectionsAgain/NodeView.swift
+++ NodeConnectionsAgain/NodeView.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class NodeView: UIView {
+  
+  var isDragging = false
+  var touchOriginalLocation = CGPoint.zero
 
   override init(frame: CGRect)
   {
@@ -30,4 +33,38 @@ class NodeView: UIView {
     layer.cornerRadius = 5.0
     layer.masksToBounds = true
   }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+  {
+    super.touchesBegan(touches, with: event)
+    guard let touchLocation = touches.first?.location(in: self) else { return }
+    touchOriginalLocation = touchLocation
+    isDragging = true
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+  {
+    super.touchesEnded(touches, with: event)
+    isDragging = false
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+  {
+    super.touchesMoved(touches, with: event)
+    if (!isDragging) { return }
+    
+    guard let newTouchLocation = touches.first?.location(in: self) else { return }
+    let deltaX = newTouchLocation.x - touchOriginalLocation.x
+    let deltaY = newTouchLocation.y - touchOriginalLocation.y
+    let newX = self.frame.origin.x + deltaX
+    let newY = self.frame.origin.y + deltaY
+    self.frame = CGRect(origin: CGPoint(x:newX, y: newY), size: self.frame.size)
+  }
+  
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?)
+  {
+    super.touchesCancelled(touches, with: event)
+    isDragging = false
+  }
+  
 }
